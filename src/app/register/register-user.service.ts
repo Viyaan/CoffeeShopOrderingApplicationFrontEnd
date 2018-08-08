0b5cb1270c1561f4a0714810b81cdb4abb81ab23
vipinkumar <vipinkumar@192.168.0.103>
2018-08-08 22:17:44 +0530

    Changes for View Order

--- src/app/register/register-user.service.ts
+++ src/app/register/register-user.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {IContact} from '../../contact-list/contact';
+
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {HttpHeaders} from '@angular/common/http';
 import {Observable, Subject} from 'rxjs';
