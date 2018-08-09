210c1457a17dc6be1bc447321de047e8d10773d7
Viyaan <viyaan.86@gmail.com>
2018-08-09 22:39:56 +0530

    bug fix view orders

--- src/app/view-order/view-order.component.ts
+++ src/app/view-order/view-order.component.ts
@@ -3,24 +3,25 @@ import { ViewOrderService } from './view-order.service';
 import {IOrder} from './Order';
 
 @Component({
-  selector: 'app-view-order',
-  templateUrl: './view-order.component.html',
-  styleUrls: ['./view-order.component.css']
+    selector: 'app-view-order',
+    templateUrl: './view-order.component.html',
+    styleUrls: ['./view-order.component.css']
 })
 export class ViewOrderComponent implements OnInit {
-orderLists: IOrder;
-  errorMessage: string;
-  constructor(private viewOrderService : ViewOrderService ) { }
+    orderLists: IOrder;
+    errorMessage: string;
+    constructor(private viewOrderService: ViewOrderService) { }
 
-  ngOnInit() {
-    this.fetchOrders();
-  }
-  
-  fetchOrders() {
-    this.viewOrderService.fetchOrders().subscribe((orders) => { this.orderLists = orders,
-                                                                     (error) => this.errorMessage = error });
-  }
-  
-  
+    ngOnInit() {
+        this.fetchOrders();
+    }
 
+    fetchOrders() {
+        this.viewOrderService.fetchOrders().subscribe((orders) => {
+        this.orderLists = orders,
+            console.log(this.orderLists[0].orderId);
+            (error) => this.errorMessage = error  
+        }
+        );
+    }
 }
